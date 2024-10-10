79dc7c672aec3fca476af6f658b2f5500f45a6d3
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-10-10 13:29:58 +0300

    Refactor HTML and JavaScript code to add "Get List" button for API integration

--- 2024/October/10-10-24/main.js
+++ 2024/October/10-10-24/main.js
@@ -2,10 +2,12 @@ console.log('main.js');
 
 
 const formElement = document.querySelector("form")
-// console.log(formElement);
-
 const containerElement = document.querySelector(".container")
-console.log(containerElement);
+const listBtnElement = document.querySelector("#get-list")
+
+// console.log(formElement);
+// console.log(containerElement);
+console.log(listBtnElement);
 
 function formSubmit (formObejct) {
     // console.log(formObejct.number);
@@ -20,6 +22,18 @@ function formSubmit (formObejct) {
     containerElement.appendChild(imageElement);
 }
 
+const getDuckList = () => {
+    fetch('https://random-d.uk/api/v2/list', {
+        AccessControlAllowOrigin: '*'
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            console.log('Images:', data.images);
+            console.log('GIFs:', data.gifs);
+        });
+}
+
 formElement.addEventListener("submit", (e) => {
     e.preventDefault()
     const formData = new FormData(formElement);
@@ -29,4 +43,8 @@ formElement.addEventListener("submit", (e) => {
     // console.log(formObject);
 
     formSubmit(formObject);
+})
+
+listBtnElement.addEventListener("click", () => {
+    getDuckList()
 })
