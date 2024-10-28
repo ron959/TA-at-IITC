473f51da505ddf6614fd09d6041b096041cb42c4
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-10-28 11:23:26 +0200

    Refactor API integration in main.js and add post list rendering in index.html

--- 2024/October/27-10-24/MyBigAPI/app.js
+++ 2024/October/27-10-24/MyBigAPI/app.js
@@ -1,7 +1,12 @@
-const express = require('express');
+import express from "express"
+import { add } from "./utils/math.js"
+
 const app = express();
 
 app.get('/', (req, res) => {
+    // console.log(add(2, 3));
+    console.log(req);
+    
     res.send('Hello World');
 });
 
