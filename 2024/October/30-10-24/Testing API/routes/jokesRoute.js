0aa30e6e1b1336adde3c61cf756fb739b355ccf8
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-10-31 14:45:54 +0200

    Add dotenv support, update .gitignore, and implement jokes controller with CRUD operations

--- 2024/October/30-10-24/Testing API/routes/jokesRoute.js
+++ 2024/October/30-10-24/Testing API/routes/jokesRoute.js
@@ -1,142 +1,37 @@
-import express from "express"
+import express from "express";
+import { validateJoke } from "../middleware/validator.js";
 
-import { validateJoke } from "../middleware/validator.js"
+import {
+  getAllJokes,
+  addNewJoke,
+  getRandomJoke,
+  getJokeByID,
+  partialJokeUpdate,
+  fullJokeUpdate,
+  deleteJoke
+} from "../controllers/jokesController.js";
 
-import Joke from '../models/jokeModel.js'
-
-const router = express.Router()
+const router = express.Router();
 
 // Get All Jokes
-// DONE: WORKING
-router.get('/all', async (req, res) => {
-    try {
-        const jokes = await Joke.find({})
-
-        if (jokes.length === 0) {
-            res.status(404).send({
-                message: "Add Some Jokes -  ימצחיק"
-            })
-        }
-        res.send(jokes)
-    } catch (error) {
-        res.status(500).send("Unknown Server Error")
-    }
-})
+router.get("/all", getAllJokes);
 
 // Add a new Joke
-// DONE: WORKING
-router.post("/", validateJoke, async (req, res) => {
-    try {
-        const { setup, punchline } = req.body
-
-        const newJoke = await new Joke({
-            setup,
-            punchline
-        }).save()
-    
-        res.status(201).json({
-            message: "New joke added",
-            joke: newJoke
-        })
-
-    } catch (error) {
-        res.status(500).send("Unknown Server Error")
-    }
-})
+router.post("/", validateJoke, addNewJoke);
 
 // Get a random Joke
-// DONE: WORKING
-router.get('/random', async (req, res) => {
-    const randomJoke = await Joke.aggregate([
-        {
-            $sample: {
-                size: 1
-            }
-        }
-    ])
-
-    res.status(200).send(randomJoke[0])
-})
+router.get("/random", getRandomJoke);
 
 // Get a Joke by ID
-router.get("/:id", async (req, res) => {
-    try {
-        const { id } = req.params
-        const foundJoke = await Joke.findById(id)
-        
-        if (!foundJoke) {
-            res.status(404).send({
-                message: `NO Joke found with id: ${id}`
-            })
-        }
-
-        res.send(foundJoke)
-    } catch (error) {
-        res.status(500)
-    }
-})
+router.get("/:id", getJokeByID);
 
 // Update a Joke
-router.patch("/:id", async (req, res) => {
-    try {
-        const { id } = req.params
-        const { setup, punchline } = req.body
-
-        const fieldsToUpdate = {}
-
-        if (setup || setup !== "") {
-            fieldsToUpdate.setup = setup
-        }
-
-        if (punchline || punchline !== "") {
-            fieldsToUpdate.punchline = punchline
-        }
-
-        const updatedJoke = await Joke.findByIdAndUpdate(id, fieldsToUpdate, {new: true})
-
-        if (!updatedJoke) {
-            res.status(404).send({
-                message: `NO Joke found with id: ${id}`
-            })
-        }
-
-        res.status(201).send(updatedJoke)
-    } catch (error) {
-        // console.log(error);
-        res.status(500)
-    }
-})
+router.patch("/:id", partialJokeUpdate);
 
 // Update Full Joke
-router.put('/:id', validateJoke, async (req, res) => {
-    try {
-        const { id } = req.params
-        const updatedJoke = await Joke.findByIdAndUpdate(
-            id, 
-            req.body, 
-            {new: true}
-        )
-
-        res.status(201).send(updatedJoke)
-    } catch (error) {
-        res.status(500)
-    }
-})
-
+router.put("/:id", validateJoke, fullJokeUpdate);
 
 // Delete Joke
-router.delete("/:id", async (req, res) => {
-    const { id } = req.params
+router.delete("/:id", deleteJoke);
 
-    try {
-        await Joke.findByIdAndDelete(id)
-        res.status(200).send({
-            message: "Joke Succefully Delete"
-        })
-
-    } catch (error) {
-        
-    }
-})
-
-export default router
+export default router;
